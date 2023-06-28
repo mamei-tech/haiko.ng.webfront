7cf1a8695a22d420946300b312b6dc9cb1273be8
dani-fmena <danif-mena@yandex.com>
2023-06-28 14:31:46 -0400

    Extending CardStatics component
    Signed-off-by: dani-fmena <danif-mena@yandex.com>

--- src/components/cards/CmpCardStats.vue
+++ src/components/cards/CmpCardStats.vue
@@ -6,13 +6,31 @@
                     <slot name="icon"> <i :class="icon"></i> </slot>
                 </div>
             </div>
-            <div class="col-7" v-if="$slots.content || title || subTitle">
+            <div class="col-7" v-if="title || subTitle">
                 <div class="numbers">
+                    <!-- !!! this is the default slot (no name on it) -->
                     <slot>
                         <p v-if="subTitle" class="card-category">{{ subTitle }}</p>
                         <h3 v-if="title" class="card-title">{{ title }}</h3>
                     </slot>
                 </div>
+            </div>
+        </div>
+
+        <div class="row" v-if="$slots.content">
+            <!-- !!! this should be the default but isn't, so you have to use the name in a component if u want to use it -->
+            <slot name="content"></slot>
+        </div>
+
+        <div class="row" v-if="info || withRestore">
+            <div class="col-10" v-if="info">
+                {{ info }}
+            </div>
+            <div class="col-12" v-if="withRestore && !info" @click.prevent="$emit('doRestore')">
+                <i class="tim-icons icon-refresh-01" style="float: right"></i>
+            </div>
+            <div class="col-2" v-else @click.prevent="$emit('doRestore')">
+                <i class="tim-icons icon-refresh-01" style="float: right"></i>
             </div>
         </div>
 
@@ -36,14 +54,24 @@ export default defineComponent({
         CmpCard
     },
     props:      {
-        type:     {
+        type:        {
             type:    String,
             default: 'primary'
         },
-        icon:     String,
-        title:    String,
-        subTitle: String
-    }
+        icon:        String,
+        title:       String,
+        subTitle:    String,
+        info:        {
+            type:        String,
+            description: 'Additional information'
+        },
+        withRestore: {
+            type:        Boolean,
+            default:     false,
+            description: 'Say if we need the _restore_ button and the emission of the event'
+        }
+    },
+    emits:      [ 'doRestore' ]
 })
 </script>
 
