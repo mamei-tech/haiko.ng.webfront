d73368e8c918902629c8443acc545b05944e7c58
dani-fmena <danif-mena@yandex.com>
2023-11-07 20:22:06 -0500

    Emitting the change event, so someone can react to it.
    Signed-off-by: dani-fmena <danif-mena@yandex.com>

--- src/components/inputs/CmpVeeCheckbox.vue
+++ src/components/inputs/CmpVeeCheckbox.vue
@@ -19,37 +19,37 @@
     import { useField } from "vee-validate"
     import { defineComponent } from 'vue'
 
-
     /*
     * U may wonder why we have two CheckBox components. This one comes in handy when we need to used alongside
     * a form integrated with
     * */
     export default defineComponent({
-        name: 'CmpVeeCheckbox',
+        name:  'CmpVeeCheckbox',
         props: {
-            name: {
-                type: String,
-                description: "The name of the input"
+            name:    {
+                type:        String,
+                description: 'The name of the input'
             },
             checked: {
-                type: Boolean,
-                description: "Input value"
+                type:        Boolean,
+                description: 'Input value'
             },
-            labels: {
-                type: Array,
-                description: "Labels for when the value is true (position 0) or when is false (position 1)"
+            labels:  {
+                type:        Array,
+                description: 'Labels for when the value is true (position 0) or when is false (position 1)'
             }
         },
-        setup (props, _) {
+        emits: [ 'chkboxchange' ],
+        setup( props: any, context: any ) {
 
             //region ======== DECLARATIONS & LOCAL STATE ============================================
-            const { value, handleChange } = useField(props.name as string, "", { initialValue: props.checked })
+            const { value, handleChange } = useField(props.name as string, '', { initialValue: props.checked })
             //endregion =============================================================================
 
             //region ======== EVENTS HANDLERS & WATCHERS ============================================
-            const hdlChange = (evt: Event) => {
-                // @ts-ignore
+            const hdlChange = ( evt: any ) => {
                 handleChange(evt.target!.checked)
+                context.emit('chkboxchange', evt.target!.checked)
             }
             //endregion =============================================================================
 
